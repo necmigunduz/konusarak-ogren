9e701af9417a0296d43c3104564311ad51530daa
necmigunduz <necmigunduz@gmail.com>
2022-06-14 13:52:58 +0300

    Add reducer and store

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const namesApi = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://rickandmortyapi.com/api'
+    }),
+    endpoints: (builder) => ({
+        names: builder.query({
+            query: () => '/episode'
+        })
+    })
+});
+
+export const { useNamesQuery } = namesApi;
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { namesApi } from "../api/api";
+
+export const store = configureStore({
+    reducer: {
+        [namesApi.reducerPath]: namesApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(namesApi.middleware)
+});
+
